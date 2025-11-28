fbd6a34fbad76f70512598bf54f92475fd8693d1
gagan-1308 <moneytalks5588@gmail.com>
2025-11-28 21:38:36 +0530

    a

---
+++ first assignment.py
@@ -0,0 +1,14 @@
+first_number = int(input("Enter first number: "))
+second_number = int(input("Enter second number: "))
+
+a=int(first_number+second_number)
+b=int(first_number-second_number)
+c=int(first_number*second_number)
+d=int(first_number/second_number)
+
+print("ADDITION:", a)
+print("SUBTRACTION:", b)
+print("MULTIPLICATION:", c)
+print("DIVISION:", d)
+
+
