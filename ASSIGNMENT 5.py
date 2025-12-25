67d7a3bb5fe665ce6d5ffa074ef74382bc734440
gagan-1308 <moneytalks5588@gmail.com>
2025-12-25 22:55:26 +0530

    README.md

---
+++ ASSIGNMENT 5.py
@@ -0,0 +1,22 @@
+#TASK_1
+#Create a dictionary of student names and marks
+student_marks = {
+        "Alice": 85,
+        "Bob": 78,
+        "Charlie": 92,
+        "David": 67,
+        "Eva": 90}
+name = input("Enter the student's name: ") #Ask the user for a student's name
+if name in student_marks:   #Retrieve marks or show error message
+    print(f"{name}'s marks: {student_marks[name]}")
+else:
+    print(f"Error: Student '{name}' not found in the records.")
+
+#TASK_2
+#Create a list of numbers from 1 to 10
+numbers = list(range(1, 11))
+extracted = numbers[:5] #Extract the first five elements using slicing
+reversed_list = extracted[::-1] #Reverse the extracted elements using slicing
+print("Original list:", numbers)
+print("Extracted first five elements:", extracted)
+print("Reversed extracted list:", reversed_list)
